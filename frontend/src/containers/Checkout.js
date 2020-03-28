ec4802a2c2adcf2f0f48d5a7b31ebca901cae53b
Raf Formes <rafformes@gmail.com>
2020-03-28 15:43:29 +0800

    fixed class errors

--- frontend/src/containers/Checkout.js
+++ frontend/src/containers/Checkout.js
@@ -3,255 +3,264 @@ import React from 'react';
 const Checkout = () => {
   return (
     <div>
-      <main class='mt-5 pt-4'>
-        <div class='container wow fadeIn'>
-          <h2 class='my-5 h2 text-center'>Checkout form</h2>
+      <main className='mt-5 pt-4'>
+        <div className='container wow fadeIn'>
+          <h2 className='my-5 h2 text-center'>Checkout form</h2>
 
-          <div class='row'>
-            <div class='col-md-8 mb-4'>
-              <div class='card'>
-                <form class='card-body'>
-                  <div class='row'>
-                    <div class='col-md-6 mb-2'>
-                      <div class='md-form '>
+          <div className='row'>
+            <div className='col-md-8 mb-4'>
+              <div className='card'>
+                <form className='card-body'>
+                  <div className='row'>
+                    <div className='col-md-6 mb-2'>
+                      <div className='md-form '>
                         <input
                           type='text'
                           id='firstName'
-                          class='form-control'
+                          className='form-control'
                         />
-                        <label for='firstName' class=''>
+                        <label htmlFor='firstName' className=''>
                           First name
                         </label>
                       </div>
                     </div>
-                    <div class='col-md-6 mb-2'>
-                      <div class='md-form'>
-                        <input type='text' id='lastName' class='form-control' />
-                        <label for='lastName' class=''>
+                    <div className='col-md-6 mb-2'>
+                      <div className='md-form'>
+                        <input
+                          type='text'
+                          id='lastName'
+                          className='form-control'
+                        />
+                        <label htmlFor='lastName' className=''>
                           Last name
                         </label>
                       </div>
                     </div>
                   </div>
 
-                  <div class='md-form input-group pl-0 mb-5'>
-                    <div class='input-group-prepend'>
-                      <span class='input-group-text' id='basic-addon1'>
+                  <div className='md-form input-group pl-0 mb-5'>
+                    <div className='input-group-prepend'>
+                      <span className='input-group-text' id='basic-addon1'>
                         @
                       </span>
                     </div>
                     <input
                       type='text'
-                      class='form-control py-0'
+                      className='form-control py-0'
                       placeholder='Username'
                       aria-describedby='basic-addon1'
                     />
                   </div>
 
-                  <div class='md-form mb-5'>
+                  <div className='md-form mb-5'>
                     <input
                       type='text'
                       id='email'
-                      class='form-control'
+                      className='form-control'
                       placeholder='youremail@example.com'
                     />
-                    <label for='email' class=''>
+                    <label htmlFor='email' className=''>
                       Email (optional)
                     </label>
                   </div>
 
-                  <div class='md-form mb-5'>
+                  <div className='md-form mb-5'>
                     <input
                       type='text'
                       id='address'
-                      class='form-control'
+                      className='form-control'
                       placeholder='1234 Main St'
                     />
-                    <label for='address' class=''>
+                    <label htmlFor='address' className=''>
                       Address
                     </label>
                   </div>
 
-                  <div class='md-form mb-5'>
+                  <div className='md-form mb-5'>
                     <input
                       type='text'
                       id='address-2'
-                      class='form-control'
+                      className='form-control'
                       placeholder='Apartment or suite'
                     />
-                    <label for='address-2' class=''>
+                    <label htmlFor='address-2' className=''>
                       Address 2 (optional)
                     </label>
                   </div>
 
-                  <div class='row'>
-                    <div class='col-lg-4 col-md-12 mb-4'>
-                      <label for='country'>Country</label>
+                  <div className='row'>
+                    <div className='col-lg-4 col-md-12 mb-4'>
+                      <label htmlFor='country'>Country</label>
                       <select
-                        class='custom-select d-block w-100'
+                        className='custom-select d-block w-100'
                         id='country'
                         required
                       >
                         <option value=''>Choose...</option>
                         <option>United States</option>
                       </select>
-                      <div class='invalid-feedback'>
+                      <div className='invalid-feedback'>
                         Please select a valid country.
                       </div>
                     </div>
 
-                    <div class='col-lg-4 col-md-6 mb-4'>
-                      <label for='state'>State</label>
+                    <div className='col-lg-4 col-md-6 mb-4'>
+                      <label htmlFor='state'>State</label>
                       <select
-                        class='custom-select d-block w-100'
+                        className='custom-select d-block w-100'
                         id='state'
                         required
                       >
                         <option value=''>Choose...</option>
                         <option>California</option>
                       </select>
-                      <div class='invalid-feedback'>
+                      <div className='invalid-feedback'>
                         Please provide a valid state.
                       </div>
                     </div>
 
-                    <div class='col-lg-4 col-md-6 mb-4'>
-                      <label for='zip'>Zip</label>
+                    <div className='col-lg-4 col-md-6 mb-4'>
+                      <label htmlFor='zip'>Zip</label>
                       <input
                         type='text'
-                        class='form-control'
+                        className='form-control'
                         id='zip'
                         placeholder=''
                         required
                       />
-                      <div class='invalid-feedback'>Zip code required.</div>
+                      <div className='invalid-feedback'>Zip code required.</div>
                     </div>
                   </div>
 
                   <hr />
 
-                  <div class='custom-control custom-checkbox'>
+                  <div className='custom-control custom-checkbox'>
                     <input
                       type='checkbox'
-                      class='custom-control-input'
+                      className='custom-control-input'
                       id='same-address'
                     />
-                    <label class='custom-control-label' for='same-address'>
+                    <label
+                      className='custom-control-label'
+                      htmlFor='same-address'
+                    >
                       Shipping address is the same as my billing address
                     </label>
                   </div>
-                  <div class='custom-control custom-checkbox'>
+                  <div className='custom-control custom-checkbox'>
                     <input
                       type='checkbox'
-                      class='custom-control-input'
+                      className='custom-control-input'
                       id='save-info'
                     />
-                    <label class='custom-control-label' for='save-info'>
+                    <label className='custom-control-label' htmlFor='save-info'>
                       Save this information for next time
                     </label>
                   </div>
 
                   <hr />
 
-                  <div class='d-block my-3'>
-                    <div class='custom-control custom-radio'>
+                  <div className='d-block my-3'>
+                    <div className='custom-control custom-radio'>
                       <input
                         id='credit'
                         name='paymentMethod'
                         type='radio'
-                        class='custom-control-input'
+                        className='custom-control-input'
                         checked
                         required
                       />
-                      <label class='custom-control-label' for='credit'>
+                      <label className='custom-control-label' htmlFor='credit'>
                         Credit card
                       </label>
                     </div>
-                    <div class='custom-control custom-radio'>
+                    <div className='custom-control custom-radio'>
                       <input
                         id='debit'
                         name='paymentMethod'
                         type='radio'
-                        class='custom-control-input'
+                        className='custom-control-input'
                         required
                       />
-                      <label class='custom-control-label' for='debit'>
+                      <label className='custom-control-label' htmlFor='debit'>
                         Debit card
                       </label>
                     </div>
-                    <div class='custom-control custom-radio'>
+                    <div className='custom-control custom-radio'>
                       <input
                         id='paypal'
                         name='paymentMethod'
                         type='radio'
-                        class='custom-control-input'
+                        className='custom-control-input'
                         required
                       />
-                      <label class='custom-control-label' for='paypal'>
+                      <label className='custom-control-label' htmlFor='paypal'>
                         Paypal
                       </label>
                     </div>
                   </div>
-                  <div class='row'>
-                    <div class='col-md-6 mb-3'>
-                      <label for='cc-name'>Name on card</label>
+                  <div className='row'>
+                    <div className='col-md-6 mb-3'>
+                      <label htmlFor='cc-name'>Name on card</label>
                       <input
                         type='text'
-                        class='form-control'
+                        className='form-control'
                         id='cc-name'
                         placeholder=''
                         required
                       />
-                      <small class='text-muted'>
+                      <small className='text-muted'>
                         Full name as displayed on card
                       </small>
-                      <div class='invalid-feedback'>
+                      <div className='invalid-feedback'>
                         Name on card is required
                       </div>
                     </div>
-                    <div class='col-md-6 mb-3'>
-                      <label for='cc-number'>Credit card number</label>
+                    <div className='col-md-6 mb-3'>
+                      <label htmlFor='cc-number'>Credit card number</label>
                       <input
                         type='text'
-                        class='form-control'
+                        className='form-control'
                         id='cc-number'
                         placeholder=''
                         required
                       />
-                      <div class='invalid-feedback'>
+                      <div className='invalid-feedback'>
                         Credit card number is required
                       </div>
                     </div>
                   </div>
-                  <div class='row'>
-                    <div class='col-md-3 mb-3'>
-                      <label for='cc-expiration'>Expiration</label>
+                  <div className='row'>
+                    <div className='col-md-3 mb-3'>
+                      <label htmlFor='cc-expiration'>Expiration</label>
                       <input
                         type='text'
-                        class='form-control'
+                        className='form-control'
                         id='cc-expiration'
                         placeholder=''
                         required
                       />
-                      <div class='invalid-feedback'>
+                      <div className='invalid-feedback'>
                         Expiration date required
                       </div>
                     </div>
-                    <div class='col-md-3 mb-3'>
-                      <label for='cc-expiration'>CVV</label>
+                    <div className='col-md-3 mb-3'>
+                      <label htmlFor='cc-expiration'>CVV</label>
                       <input
                         type='text'
-                        class='form-control'
+                        className='form-control'
                         id='cc-cvv'
                         placeholder=''
                         required
                       />
-                      <div class='invalid-feedback'>Security code required</div>
+                      <div className='invalid-feedback'>
+                        Security code required
+                      </div>
                     </div>
                   </div>
-                  <hr class='mb-4' />
+                  <hr className='mb-4' />
                   <button
-                    class='btn btn-primary btn-lg btn-block'
+                    className='btn btn-primary btn-lg btn-block'
                     type='submit'
                   >
                     Continue to checkout
@@ -260,59 +269,59 @@ const Checkout = () => {
               </div>
             </div>
 
-            <div class='col-md-4 mb-4'>
-              <h4 class='d-flex justify-content-between align-items-center mb-3'>
-                <span class='text-muted'>Your cart</span>
-                <span class='badge badge-secondary badge-pill'>3</span>
+            <div className='col-md-4 mb-4'>
+              <h4 className='d-flex justify-content-between align-items-center mb-3'>
+                <span className='text-muted'>Your cart</span>
+                <span className='badge badge-secondary badge-pill'>3</span>
               </h4>
 
-              <ul class='list-group mb-3 z-depth-1'>
-                <li class='list-group-item d-flex justify-content-between lh-condensed'>
+              <ul className='list-group mb-3 z-depth-1'>
+                <li className='list-group-item d-flex justify-content-between lh-condensed'>
                   <div>
-                    <h6 class='my-0'>Product name</h6>
-                    <small class='text-muted'>Brief description</small>
+                    <h6 className='my-0'>Product name</h6>
+                    <small className='text-muted'>Brief description</small>
                   </div>
-                  <span class='text-muted'>$12</span>
+                  <span className='text-muted'>$12</span>
                 </li>
-                <li class='list-group-item d-flex justify-content-between lh-condensed'>
+                <li className='list-group-item d-flex justify-content-between lh-condensed'>
                   <div>
-                    <h6 class='my-0'>Second product</h6>
-                    <small class='text-muted'>Brief description</small>
+                    <h6 className='my-0'>Second product</h6>
+                    <small className='text-muted'>Brief description</small>
                   </div>
-                  <span class='text-muted'>$8</span>
+                  <span className='text-muted'>$8</span>
                 </li>
-                <li class='list-group-item d-flex justify-content-between lh-condensed'>
+                <li className='list-group-item d-flex justify-content-between lh-condensed'>
                   <div>
-                    <h6 class='my-0'>Third item</h6>
-                    <small class='text-muted'>Brief description</small>
+                    <h6 className='my-0'>Third item</h6>
+                    <small className='text-muted'>Brief description</small>
                   </div>
-                  <span class='text-muted'>$5</span>
+                  <span className='text-muted'>$5</span>
                 </li>
-                <li class='list-group-item d-flex justify-content-between bg-light'>
-                  <div class='text-success'>
-                    <h6 class='my-0'>Promo code</h6>
+                <li className='list-group-item d-flex justify-content-between bg-light'>
+                  <div className='text-success'>
+                    <h6 className='my-0'>Promo code</h6>
                     <small>EXAMPLECODE</small>
                   </div>
-                  <span class='text-success'>-$5</span>
+                  <span className='text-success'>-$5</span>
                 </li>
-                <li class='list-group-item d-flex justify-content-between'>
+                <li className='list-group-item d-flex justify-content-between'>
                   <span>Total (USD)</span>
                   <strong>$20</strong>
                 </li>
               </ul>
 
-              <form class='card p-2'>
-                <div class='input-group'>
+              <form className='card p-2'>
+                <div className='input-group'>
                   <input
                     type='text'
-                    class='form-control'
+                    className='form-control'
                     placeholder='Promo code'
                     aria-label="Recipient's username"
                     aria-describedby='basic-addon2'
                   />
-                  <div class='input-group-append'>
+                  <div className='input-group-append'>
                     <button
-                      class='btn btn-secondary btn-md waves-effect m-0'
+                      className='btn btn-secondary btn-md waves-effect m-0'
                       type='button'
                     >
                       Redeem
--- frontend/src/containers/Products.js
+++ frontend/src/containers/Products.js
@@ -3,39 +3,39 @@ import React from 'react';
 const Product = () => {
   return (
     <div>
-      <main class='mt-5 pt-4'>
-        <div class='container dark-grey-text mt-5'>
-          <div class='row wow fadeIn'>
-            <div class='col-md-6 mb-4'>
+      <main className='mt-5 pt-4'>
+        <div className='container dark-grey-text mt-5'>
+          <div className='row wow fadeIn'>
+            <div className='col-md-6 mb-4'>
               <img
                 src='https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/14.jpg'
-                class='img-fluid'
+                className='img-fluid'
                 alt=''
               />
             </div>
 
-            <div class='col-md-6 mb-4'>
-              <div class='p-4'>
-                <div class='mb-3'>
+            <div className='col-md-6 mb-4'>
+              <div className='p-4'>
+                <div className='mb-3'>
                   <a href=''>
-                    <span class='badge purple mr-1'>Category 2</span>
+                    <span className='badge purple mr-1'>Category 2</span>
                   </a>
                   <a href=''>
-                    <span class='badge blue mr-1'>New</span>
+                    <span className='badge blue mr-1'>New</span>
                   </a>
                   <a href=''>
-                    <span class='badge red mr-1'>Bestseller</span>
+                    <span className='badge red mr-1'>Bestseller</span>
                   </a>
                 </div>
 
-                <p class='lead'>
-                  <span class='mr-1'>
+                <p className='lead'>
+                  <span className='mr-1'>
                     <del>$200</del>
                   </span>
                   <span>$100</span>
                 </p>
 
-                <p class='lead font-weight-bold'>Description</p>
+                <p className='lead font-weight-bold'>Description</p>
 
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
@@ -44,17 +44,20 @@ const Product = () => {
                   repellendus maiores quia sapiente.
                 </p>
 
-                <form class='d-flex justify-content-left'>
+                <form className='d-flex justify-content-left'>
                   <input
                     type='number'
                     value='1'
                     aria-label='Search'
-                    class='form-control'
+                    className='form-control'
                     style={{ width: '100px' }}
                   />
-                  <button class='btn btn-primary btn-md my-0 p' type='submit'>
+                  <button
+                    className='btn btn-primary btn-md my-0 p'
+                    type='submit'
+                  >
                     Add to cart
-                    <i class='fas fa-shopping-cart ml-1'></i>
+                    <i className='fas fa-shopping-cart ml-1'></i>
                   </button>
                 </form>
               </div>
@@ -63,9 +66,9 @@ const Product = () => {
 
           <hr />
 
-          <div class='row d-flex justify-content-center wow fadeIn'>
-            <div class='col-md-6 text-center'>
-              <h4 class='my-4 h4'>Additional information</h4>
+          <div className='row d-flex justify-content-center wow fadeIn'>
+            <div className='col-md-6 text-center'>
+              <h4 className='my-4 h4'>Additional information</h4>
 
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
@@ -76,27 +79,27 @@ const Product = () => {
             </div>
           </div>
 
-          <div class='row wow fadeIn'>
-            <div class='col-lg-4 col-md-12 mb-4'>
+          <div className='row wow fadeIn'>
+            <div className='col-lg-4 col-md-12 mb-4'>
               <img
                 src='https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/11.jpg'
-                class='img-fluid'
+                className='img-fluid'
                 alt=''
               />
             </div>
 
-            <div class='col-lg-4 col-md-6 mb-4'>
+            <div className='col-lg-4 col-md-6 mb-4'>
               <img
                 src='https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/12.jpg'
-                class='img-fluid'
+                className='img-fluid'
                 alt=''
               />
             </div>
 
-            <div class='col-lg-4 col-md-6 mb-4'>
+            <div className='col-lg-4 col-md-6 mb-4'>
               <img
                 src='https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/13.jpg'
-                class='img-fluid'
+                className='img-fluid'
                 alt=''
               />
             </div>
