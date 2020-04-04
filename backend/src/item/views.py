5135a30f2eb23a221c172f0d4156b22127a8cafa
Raf Formes <rafformes@gmail.com>
2020-04-05 00:01:59 +0800

    temporary set item view to allow any

--- backend/src/item/views.py
+++ backend/src/item/views.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 
 from item.models import Item
 from item.serializers import ItemSerializer
+from rest_framework.permissions import AllowAny
 
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = [AllowAny]
